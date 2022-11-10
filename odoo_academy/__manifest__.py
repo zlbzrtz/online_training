09f44b1cad3ffb00a5fa5ad418e153bbd85dc7bd
Alberto <contacto@analiticadelfos.com>
2022-11-10 05:57:36 +0000

    Manifest created

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': 'Academy app to manage training',
+    'description': 'Academy :v',
+    'author': 'Alberto',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ]
+}
