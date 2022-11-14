49c05085acc87fd1fdf858f01de1caf0d33efad5
Alberto <alberto@analiticadelfos.com>
2022-11-14 02:48:19 +0000

    Coding

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage training""",
+    
+    'description': """
+        Academy module to manage training.
+    """,
+    
+    'author': "Yo",
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.0.2',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ]
+}
